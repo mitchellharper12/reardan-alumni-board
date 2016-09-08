b3b115bdd68ee2be4b4c2d35a083881a3e044f95
Mitchell Harper <mitchwharper@gmail.com>
2016-09-08 01:26:04 -0700

    Add (admittedly fairly insecure implementation of) basic auth
    Adds a layer of basic auth to prevent automated scripts from wreaking
    havoc. Yes, it uses hard-coded, on disk values. And yes, the secrets.yml
    file was originally committed. But there's nothing sensitive there.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  http_basic_authenticate_with name: Rails.application.secrets.basic_auth_user, password: Rails.application.secrets.basic_auth_password
 end
